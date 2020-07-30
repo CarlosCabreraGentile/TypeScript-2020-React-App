7f8add8894d36dd35eb9642a076e4b39f5cd2d42
Carlos Cabrera Gentile <carloscgentile@gmail.com>
2020-07-30 16:50:11 -0300

    180: communication between components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import TodoList from './components/TodoList';
+import NewTodo from './components/NewTodo';
 
 function App() {
+  const todos = [{ id: 't1', text: 'text 1' }];
+
+  const todoAddHandler = (text: string) => {
+    console.log(text);
+  }
+
   return (
     <div className="App">
+      <NewTodo onAddTodo={todoAddHandler} />
+      <TodoList items={todos} />
     </div>
   );
 }
